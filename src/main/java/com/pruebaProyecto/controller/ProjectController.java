03f0107ef56b89d5a72b870c34fcabcbc73bfe87
Sergio <alonsodecaso.sergio@gmail.com>
2021-05-20 17:45:43 +0200

    Añadido de anotaciones para funcionar con angular

--- src/main/java/com/pruebaProyecto/controller/ProjectController.java
+++ src/main/java/com/pruebaProyecto/controller/ProjectController.java
@@ -7,8 +7,10 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -26,6 +28,7 @@ import com.pruebaProyecto.service.ProyectService;
 import lombok.extern.log4j.Log4j;
 @Log4j
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/project")
 public class ProjectController {
 	
